f2b50c558c34f8d826307e533c4fe90856883df9
Vektor <whiteman.chr@hotmail.com>
2021-09-30 09:20:29 +0200

    create github action

--- release.bat
+++ release.bat
@@ -1,2 +1,2 @@
-cmake -B BUILD/VS2019/BIN/RELEASE --target voice-filters .
-cmake --build BUILD/VS2019/BIN/RELEASE --target voice-filters --config Release
+cmake -B BUILD/VS2019 --target voice-filters .
+cmake --build BUILD/VS2019 --target voice-filters --config Release
