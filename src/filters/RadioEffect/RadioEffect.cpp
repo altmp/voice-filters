39b86b971bc5ce1378aec0e3687b5f6dbc838ecc
Mohammad Reza Gharabaghi <40915667+mrgharabaghi@users.noreply.github.com>
2021-12-27 21:32:14 +0330

    Update

--- src/filters/RadioEffect/RadioEffect.cpp
+++ src/filters/RadioEffect/RadioEffect.cpp
@@ -1,5 +1,4 @@
 #include "RadioEffect.h"
-#include "../../lib/DSPFilters/headers/Dsp.h"
 
 #include <sstream>
 #include <iostream>
@@ -9,20 +8,8 @@ using namespace alt;
 using namespace Dsp;
 
 void RadioEffect::Process(pcmSample* data, uint32_t size) {
-    Dsp::Filter* f = new Dsp::FilterDesign
-        <Dsp::ChebyshevII::Design::BandPass <5>, 2>;
-    Dsp::Params params;
-    params[0] = 44100; // Sample rate
-    params[1] = 5; // Order
-    params[2] = 7000; // Corner frequency
-    params[3] = 600; // Shelf gain
-    params[4] = 0.1; // Passband ripple
-    f->setParams(params);
-
     int numSamples = size / 2;
-    float* audioData[2];
-    audioData[0] = new float[1];
-    audioData[1] = new float[1];
+    Init();
 
     for (int i = 0; i < numSamples; i++) {
         audioData[0][0] = data[i].channels.right;
@@ -33,4 +20,18 @@ void RadioEffect::Process(pcmSample* data, uint32_t size) {
         data[i].channels.right = audioData[0][0];
         data[i].channels.left = audioData[1][0];
     }
+}
+
+void RadioEffect::Init() {
+    Dsp::Params params;
+    params[0] = 44100; // Sample rate
+    params[1] = 5; // Order
+    params[2] = 7000; // Corner frequency
+    params[3] = 600; // Shelf gain
+    params[4] = 0.1; // Passband ripple
+
+    f->setParams(params);
+
+    audioData[0] = new float[1];
+    audioData[1] = new float[1];
 }
--- src/filters/RadioEffect/RadioEffect.h
+++ src/filters/RadioEffect/RadioEffect.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "main.h"
+#include "../../lib/DSPFilters/headers/Dsp.h"
 
 class RadioEffect : public alt::IAudioFilter {
 	public:
@@ -8,4 +9,10 @@ class RadioEffect : public alt::IAudioFilter {
 		~RadioEffect() {};
 
 		void Process(pcmSample* data, uint32_t size) override;
+
+	private:
+		Dsp::Filter* f = new Dsp::FilterDesign<Dsp::ChebyshevII::Design::BandPass <5>, 2>;
+		float* audioData[2];
+
+		void Init();
 };
